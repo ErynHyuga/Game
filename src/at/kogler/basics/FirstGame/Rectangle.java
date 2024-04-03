dc3c42babe75b8d22832595584d01903c6bb2642
ErynHyuga <you@example.com>
2024-04-03 09:33:47 +0200

    game 3,working on oval

--- src/at/kogler/basics/FirstGame/Rectangle.java
+++ src/at/kogler/basics/FirstGame/Rectangle.java
@@ -1,25 +1,56 @@
 package at.kogler.basics.FirstGame;
 
 
-import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.SlickException;
+import org.newdawn.slick.*;
 
 public class Rectangle extends BasicGame {
-
-    @Override
-    public void init(GameContainer gameContainer) throws SlickException {
-
+    private int textSpeedInX;
+    private int textSpeedInY;
+    private int oval1SpeedInX;
+    private int oval1SpeedInY;
+    private int oval2SpeedInX;
+    private int oval2SpeedInY;
+    private float speed;
+    public Rectangle(String title) {
+        super(title);
     }
 
     @Override
-    public void update(GameContainer gameContainer, int i) throws SlickException {
+    public void init(GameContainer gameContainer) throws SlickException {
+        this.textSpeedInX = 10;
+        this.textSpeedInY = 25;
+        this.speed = 2;
+    }
 
+    @Override
+    public void update(GameContainer gameContainer, int delta) throws SlickException {
+
+
+        this.textSpeedInY += (double)delta/speed;
+        this.textSpeedInX += (double)delta/speed;
+        if(this.textSpeedInX > 800){
+            this.textSpeedInX = 10;
+        }
+        if(this.textSpeedInY > 600){
+            this.textSpeedInY = 20;
+        }
     }
 
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+        graphics.drawString("Zäwäs", this.textSpeedInX,this.textSpeedInY);
+        graphics.drawRect(100,200,30,10);
+        graphics.drawOval(this.oval1SpeedInX, this.oval1SpeedInY,20);
+        graphics.drawOval(this.oval2SpeedInX, this.oval2SpeedInY, 40);
 
+    }
+    public static void main(String[] argv) {
+        try {
+            AppGameContainer container = new AppGameContainer(new Rectangle("Recteck"));
+            container.setDisplayMode(800,600,false);
+            container.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
     }
 }
