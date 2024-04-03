214abffbf5ff370988c2c4d96c918f9f651dd326
ErynHyuga <you@example.com>
2024-04-03 08:42:17 +0200

    inti

---
+++ src/at/kogler/basics/FirstGame/Rectangle.java
@@ -0,0 +1,25 @@
+package at.kogler.basics.FirstGame;
+
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Rectangle extends BasicGame {
+
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int i) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+
+    }
+}
