82e480ccd851c5c04e5b3f00a241f10d3e56c0a7
ErynHyuga <you@example.com>
2024-05-22 08:56:55 +0200

    finished 05

--- src/at/kogler/basics/FirstGame/Circle.java
+++ src/at/kogler/basics/FirstGame/Circle.java
@@ -1,49 +1,55 @@
 package at.kogler.basics.FirstGame;
 
-
 import org.newdawn.slick.Graphics;
 
-public class Rectangle {
-    public enum DIRECTION {RIGHT, DOWN, LEFT, UP};
-    private int x;
-    private int y;
+import java.util.Random;
+
+public class Circle {
+
+
+    private int x, y;
     private float speed;
+    private int diameter;
 
-    public Rectangle(int x, int y, float speed) {
-        this.x = x;
-        this.y = y;
-
-        this.speed = speed;
+    public Circle() {
+        Random random = new Random();
+        this.x = random.nextInt(800);
+        this.y = random.nextInt(600);
+        this.speed = random.nextInt(2) + 2;
+        this.diameter = random.nextInt(20) + 20;
     }
 
     public void render(Graphics graphics) {
-        graphics.drawRect(this.x, this.y, 20, 20);
+        graphics.drawOval(this.x, this.y, this.diameter, this.diameter);
 
     }
 
     public void update(int delta) {
-        directionUp(delta);
+        directionRight(delta);
+
+
         if (this.y >= 600) {
             this.y = 0;
         }
         if (this.x >= 800){
             this.x = 0;
         }
-
     }
 
     public void directionUp(int delta) {
         this.y += (float) delta / this.speed;
     }
+
     public void directionDown(int delta) {
         this.y -= (float) delta / this.speed;
     }
+
     public void directionRight(int delta) {
         this.x += (float) delta / this.speed;
     }
+
     public void directionLeft(int delta) {
         this.x -= (float) delta / this.speed;
     }
-
 
 }
--- src/at/kogler/basics/FirstGame/ObjectClass.java
+++ src/at/kogler/basics/FirstGame/ObjectClass.java
@@ -4,13 +4,14 @@ package at.kogler.basics.FirstGame;
 import org.newdawn.slick.*;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Random;
 import java.util.List;
 
 public class ObjectClass extends BasicGame {
 
     private List<Rectangle> rectangles;
-
+    private List<Circle> circles;
 
 
     public ObjectClass(String title) {
@@ -19,8 +20,18 @@ public class ObjectClass extends BasicGame {
 
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
-
-
+        this.circles = new ArrayList<>();
+        this.rectangles = new LinkedList<>();
+        Random random = new Random();
+        for (int i = 0; i < 100; i++) {
+            Rectangle rectangle = new Rectangle(random.nextInt(600),
+                    random.nextInt(600), random.nextInt(2));
+            rectangles.add(rectangle);
+        }
+        for (int i = 0; i < 50; i++) {
+            Circle circle = new Circle();
+            circles.add(circle);
+        }
 
     }
 
@@ -29,13 +40,19 @@ public class ObjectClass extends BasicGame {
         for (Rectangle rectangle : this.rectangles) {
             rectangle.update(delta);
         }
-        
+        for (Circle circle : this.circles) {
+            circle.update(delta);
+        }
+
     }
 
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
         for (Rectangle rectangle : this.rectangles) {
             rectangle.render(graphics);
+        }
+        for (Circle circle : this.circles) {
+            circle.render(graphics);
         }
     }
 
